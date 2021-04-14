fe1d79ffdf83f5854861a137eeeaa1f98e57be48
Sophia Song <ss6125@columbia.edu>
2021-04-14 21:06:47 +0000

    Update

--- sightings/models.py
+++ sightings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+class Data(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    unique_squirrel_id = models.CharField(unique=True, max_length=200)
+    shift = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    age = models.CharField(max_length=200, blank=True, null=True)
+    primary_fur_color = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    running = models.BooleanField(blank=True, null=True)
+    chasing = models.BooleanField(blank=True, null=True)
+    climbing = models.BooleanField(blank=True, null=True)
+    eating = models.BooleanField(blank=True, null=True)
+    foraging = models.BooleanField(blank=True, null=True)
 # Create your models here.
